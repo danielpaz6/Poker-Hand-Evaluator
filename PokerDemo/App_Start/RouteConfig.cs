af7b967fc7b0bc59468827756c7ea4a2003bdc89
Daniel Paz <danielpaz601@gmail.com>
2019-10-18 01:22:32 +0300

    seo fixes

--- PokerDemo/App_Start/RouteConfig.cs
+++ PokerDemo/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PokerDemo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
